8609ea2791105f2429e719dcd3be151b11516de1
TBoileau <t-boileau@email.com>
2023-06-27 12:28:03 +0200

    feat: modale (#13)

--- front/assets/js/index.js
+++ front/assets/js/index.js
@@ -4,11 +4,44 @@ import {
   createEditionMode,
   createLogin,
   createLogout,
+  createModal, createModalWork,
   createWork
 } from "./factories.js";
 import {getCategories, getWorks} from "./repositories.js";
 import {Category} from "./models.js";
 import {clear, isLogged} from "./security.js";
+
+const header = document.querySelector('header');
+const nav = header.querySelector('nav ul');
+const myProjectsTitle = document.querySelector('#portfolio h2');
+
+if (isLogged()) {
+  const onClick = () => {
+    clear();
+    window.location.reload();
+  };
+
+  nav.insertBefore(createLogout({onClick}), nav.querySelector('li:last-child'));
+
+  document.body.insertBefore(createEditionMode({
+    onClick: () => {
+    }
+  }), header);
+
+  const modal = createModal();
+
+  document.body.appendChild(modal);
+
+  myProjectsTitle.appendChild(
+    createEditButton({
+      onClick: () => {
+        modal.classList.add('show');
+      }
+    })
+  );
+} else {
+  nav.insertBefore(createLogin(), nav.querySelector('li:last-child'));
+}
 
 function renderWorks(works, category) {
   const worksContainer = document.querySelector('.gallery');
@@ -18,6 +51,14 @@ function renderWorks(works, category) {
     .forEach(work => {
       worksContainer.appendChild(createWork(work));
     });
+
+  if (isLogged()) {
+    const listWorks = document.querySelector('.list-works .works');
+    listWorks.innerHTML = '';
+    works.forEach(work => {
+      listWorks.appendChild(createModalWork(work));
+    });
+  }
 }
 
 const works = await getWorks();
@@ -43,22 +84,3 @@ categories.forEach(category => {
 });
 
 renderWorks(works, categories[0]);
-
-const header = document.querySelector('header');
-const nav = header.querySelector('nav ul');
-const myProjectsTitle = document.querySelector('#portfolio h2');
-
-if (isLogged()) {
-  const onClick = () => {
-    clear();
-    window.location.reload();
-  };
-
-  nav.insertBefore(createLogout({onClick}), nav.querySelector('li:last-child'));
-
-  document.body.insertBefore(createEditionMode({ onClick: () => {} }), header);
-
-  myProjectsTitle.appendChild(createEditButton({ onClick: () => {} }))
-} else {
-  nav.insertBefore(createLogin(), nav.querySelector('li:last-child'));
-}
